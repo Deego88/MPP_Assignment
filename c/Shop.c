8d3ccdd704fbafd08f0e831ff1968446a36f6ebe
Richard Deegan <60344243+Deego88@users.noreply.github.com>
2021-12-14 13:54:25 +0000

    Add files via upload
    create and stock shop & Print Prod method added. CSV read in file data added from  https://stackoverflow.com/questions/3501338/c-read-file-line-by-line/3501681#3501681

--- c/Shop.c
+++ c/Shop.c
@@ -46,3 +46,46 @@ struct Customer
   struct ProductQuantity shoppingList[10];
   int index;
 };
+
+
+// Printing information for the products using methods:
+
+// printProduct takes struct "Product" as "prod". nill return. 
+void printProduct(struct Product prod) 
+{
+  // if the price is defined then show name and price. Else show the shopping list with only product name
+  if (prod.price == 0)
+  {
+    printf("Product: %s; ", prod.name);
+  }
+  else
+  {
+    printf("Product: %s; \tPrice: €%.2f \t", prod.name, prod.price);
+}
+
+
+// Show product_price from another struct
+double get_product_price(struct Product prod) 
+{
+// Values of prod.price from another struct
+  return prod.price;
+}
+
+// Create and stock the shop. The Struct "Shop" retruns method output createAndStockShop().
+struct Shop createAndStockShop() 
+{
+  // Read in CSV data file  https://stackoverflow.com/questions/3501338/c-read-file-line-by-line/3501681#3501681
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
+  size_t read;
+
+  // Read in CSV file
+  fp = fopen("../Data/shop_stock.csv", "r"); 
+
+  // Handle errors
+  if (fp == NULL)
+  {
+    printf("File not found\n");
+    exit(EXIT_FAILURE);
+  }
