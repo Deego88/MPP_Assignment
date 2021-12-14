7210f197cd6b77ee5c4aa9173103d291fec2f0bd
Richard Deegan <60344243+Deego88@users.noreply.github.com>
2021-12-14 14:16:28 +0000

    Add files via upload
    Data processing for "Shop" struct. Reading in CSV data & placeholders.

--- c/Shop.c
+++ c/Shop.c
@@ -89,3 +89,43 @@ struct Shop createAndStockShop()
     printf("File not found\n");
     exit(EXIT_FAILURE);
   }
+
+
+
+  // read the first line  https://stackoverflow.com/questions/40066017/how-to-access-the-characters-in-getline-in-c
+  read = getline(&line, &len, fp);
+  double cashInShop = atof(line);
+  // initialise the shop "Shop" struct and save  cash in hand value
+  struct Shop shop = {cashInShop};
+
+  // Below we read each line and extract and assign certain data to correct variables.
+  // read in each line of the file to != -1 (end of file)
+  while ((read = getline(&line, &len, fp)) != -1)
+  {
+    // printf(": %s \n", line); // This is for testing if the program reads the file
+
+    // Function "strtok" is used to break down teh string with a "," https://pubs.opengroup.org/onlinepubs/007904975/functions/strtok.html
+    char *nam = strtok(line, ","); // Exctract product name and assigns to variable "name"
+    char *pri = strtok(NULL, ","); // Exctract product price 
+    char *qua = strtok(NULL, ","); // Exctract product available quantity 
+
+    // Conversion of str adn int: https://www.geeksforgeeks.org/atol-atoll-and-atof-functions-in-c-c/
+    double price = atof(pri);
+    int quantity = atoi(qua);  
+    // product name dynamically alocated memory. limited to 50 char.
+    char *name = malloc(sizeof(char) * 50);
+    // copies variable 'nam' (initialised in the line above) to string variable 'name'
+    strcpy(name, nam);                      
+
+    // assign the read values to the struct placeholders
+    struct Product product = {name, price};
+    struct ProductStock stockItem = {product, quantity};
+
+    // Data is took form the file and put into the "Shop" struct
+    shop.stock[shop.index++] = stockItem; 
+    // printf("Product: %s, €%.2f; available: %d pcs.\n", name, price, quantity); // Testing 
+  }
+
+  return shop; 
+}
+
